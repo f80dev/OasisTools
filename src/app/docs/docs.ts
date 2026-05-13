89509fbc9e60586585138ed553a503e8677efaeb
F80 <43803343+f80dev@users.noreply.github.com>
2026-05-13 11:54:57 +0200

    retour sur le portable

--- src/app/docs/docs.ts
+++ src/app/docs/docs.ts
@@ -7,24 +7,27 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelect, MatSelectChange, MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
 import {MatProgressSpinner} from '@angular/material/progress-spinner';
-import {TemplateHandler} from 'easy-template-x';
+import {FormsModule} from "@angular/forms";
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-docs',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatProgressSpinner],
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatProgressSpinner, FormsModule],
   templateUrl: './docs.html',
   styleUrl: './docs.css',
 })
 export class Docs implements OnInit {
 
   http=inject(HttpClient)
+  snackbar=inject(MatSnackBar)
   public cursus_list: any[] | undefined
   public student_list: any[] | undefined
   public selected_file: File | undefined;
   @ViewChild('studentselect') student_select: MatSelect | undefined;
   protected message=""
   public selected_cursus: any | undefined
+  public selected_students: any[] = [];
   public template: any;
   private cursus_disciplines:any
   public template_name: string=""
@@ -37,8 +40,14 @@ export class Docs implements OnInit {
       this.cursus_list=await this.get_cursus() as any[]
       localStorage.setItem("cursus",JSON.stringify(this.cursus_list))
     }
+
+
+    //Sélectionne le premier cursus
+    if (this.cursus_list && this.cursus_list.length > 0) {
+      await this.on_select_cursus({ value: this.cursus_list[0] } as MatSelectChange);
+    }
+
     this.cursus_disciplines=await this.get_maquettage()
-    //this.evals=await this.get_all_evals()
     this.template=localStorage.getItem("template")
   }
 
@@ -58,13 +67,7 @@ export class Docs implements OnInit {
   async api_doc(url:string,data:any,message="") {
     url="/api/doc"+url
     this.message=message
-    let rc: any
-    try{
-      rc = await firstValueFrom(this.http.post(url, data));
-    }catch (e){
-      console.log(rc)
-    }
-    return rc
+    return await firstValueFrom(this.http.post(url, data));
   }
 
 
@@ -100,7 +103,13 @@ export class Docs implements OnInit {
 
   async on_select_cursus($event: MatSelectChange<any>) {
     this.selected_cursus=$event.value
+    localStorage.setItem("cursus_selected",JSON.stringify(this.selected_cursus))
     this.student_list=await this.get_students(this.selected_cursus.CODE) as any[]
+    if (this.student_list && this.student_list.length > 0) {
+      this.selected_students = [this.student_list[0]];
+    } else {
+      this.selected_students = [];
+    }
     this.message=""
     // this.templates=[]
     // for (let t of await get_properties()){
@@ -181,12 +190,7 @@ export class Docs implements OnInit {
       reader.onload = () => {
         this.template=reader.result
         this.template_name=file.name.replace(".docx","")
-        try{
-          localStorage.setItem("template",this.template || "")
-        }catch (e){
-          console.log("Impossible de stocker le modele en local")
-        }
-
+        localStorage.setItem("template",this.template || "")
         resolve(true)
       }})
   }
@@ -206,46 +210,46 @@ export class Docs implements OnInit {
 
 
 
-  async generate_doc_from_frontend(to_save=true) {
-    if (!this.template || !this.student_select || this.student_select.value.length === 0) {
-      alert("Veuillez sélectionner un fichier et au moins un étudiant.");
-      return;
-    }
-
-    const templateBuffer = this.base64ToUint8Array(this.template.split(",")[1])
-
-    for(let student of this.student_select!.value){
-      this.message="Traitement de "+student.FNAME+" "+student.LNAME
-      student=await this.complete_student(student)
-
-      let rc:any={}
-      for(let k in student)
-        rc["STUDENT_"+k]=translate_to_openxml(clear_text(student[k]))
-
-      for(let k in this.selected_cursus)
-        rc["CURSUS_"+k]=translate_to_openxml(clear_text(this.selected_cursus[k]))
-
-      //let disciplines=await this.api("/modules/"+this.selected_cursus.CODE+"/moduleCourses")
-
-      try {
-
-        //Voir la documentation : https://templatedocs.io/docs/intro
-        this.message="Production du document"
-        const doc=await new TemplateHandler().process(templateBuffer,rc)
-
-        saveDataToFile(
-          doc,
-          this.template_name+"_"+student.LNAME+"_"+student.FNAME+".docx",
-          'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        )
-        //if(to_save)saveAs(new Blob([out.buffer]), `document_${student.LNAME}_${student.FNAME}.docx`);
-        this.clear_form()
-      } catch (error) {
-        console.error("Erreur lors de la génération du document pour " + student.FNAME, error);
-      }
-    }
-    this.message=""
-  }
+  // async generate_doc_from_frontend(to_save=true) {
+  //   if (!this.template || !this.student_select || this.student_select.value.length === 0) {
+  //     alert("Veuillez sélectionner un fichier et au moins un étudiant.");
+  //     return;
+  //   }
+  //
+  //   const templateBuffer = this.base64ToUint8Array(this.template.split(",")[1])
+  //
+  //   for(let student of this.student_select!.value){
+  //     this.message="Traitement de "+student.FNAME+" "+student.LNAME
+  //     student=await this.complete_student(student)
+  //
+  //     let rc:any={}
+  //     for(let k in student)
+  //       rc["STUDENT_"+k]=translate_to_openxml(clear_text(student[k]))
+  //
+  //     for(let k in this.selected_cursus)
+  //       rc["CURSUS_"+k]=translate_to_openxml(clear_text(this.selected_cursus[k]))
+  //
+  //     //let disciplines=await this.api("/modules/"+this.selected_cursus.CODE+"/moduleCourses")
+  //
+  //     try {
+  //
+  //       //Voir la documentation : https://templatedocs.io/docs/intro
+  //       this.message="Production du document"
+  //       const doc=await new TemplateHandler().process(templateBuffer,rc)
+  //
+  //       saveDataToFile(
+  //         doc,
+  //         this.template_name+"_"+student.LNAME+"_"+student.FNAME+".docx",
+  //         'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+  //       )
+  //       //if(to_save)saveAs(new Blob([out.buffer]), `document_${student.LNAME}_${student.FNAME}.docx`);
+  //       this.clear_form()
+  //     } catch (error) {
+  //       console.error("Erreur lors de la génération du document pour " + student.FNAME, error);
+  //     }
+  //   }
+  //   this.message=""
+  // }
 
   saveBase64AsDocx(base64Data: string, fileName: string) {
     const byteCharacters = atob(base64Data);
@@ -271,15 +275,15 @@ export class Docs implements OnInit {
   };
 
 
-  async generate_doc(to_save=true) {
-    if (!this.template || !this.student_select || this.student_select.value.length === 0) {
+  async generate_doc() {
+    if (!this.template || !this.selected_students || this.selected_students.length === 0) {
       alert("Veuillez sélectionner un fichier et au moins un étudiant.");
       return;
     }
 
     const templateBuffer:string = this.template.split(",")[1]
 
-    for(let student of this.student_select!.value){
+    for(let student of this.selected_students){
       this.message="Traitement de "+student.FNAME+" "+student.LNAME
 
       let cache=localStorage.getItem(this.selected_cursus+"_"+student.LNAME)
@@ -304,12 +308,16 @@ export class Docs implements OnInit {
 
         //Voir la documentation : https://templatedocs.io/docs/intro
         this.message="Production du document"
-        const doc=await this.api_doc("/merge-docx/",{data:rc,template:templateBuffer})
 
-        this.saveBase64AsDocx(doc.document_base64,this.template_name+"_"+student.LNAME+"_"+student.FNAME+".docx")
+        try{
+          const doc:any=await this.api_doc("/merge-docx/",{data:rc,template:templateBuffer})
+          this.saveBase64AsDocx(doc.document_base64,this.template_name+"_"+student.LNAME+"_"+student.FNAME+".docx")
+        }catch (e){
+          this.snackbar.open("Probleme avec le template","Ok")
+        }
 
-        //if(to_save)saveAs(new Blob([out.buffer]), `document_${student.LNAME}_${student.FNAME}.docx`);
-        this.clear_form()
+
+
       } catch (error) {
         console.error("Erreur lors de la génération du document pour " + student.FNAME, error);
       }
